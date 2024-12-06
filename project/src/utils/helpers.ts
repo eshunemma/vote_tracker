f7353e483a24e50897f3b5cc1a597610a973ad97
Emmanuel Eshun <emmanuel.eshun@amalitech.com>
2024-12-06 14:31:51 +0000

    done

--- project/src/utils/helpers.ts
+++ project/src/utils/helpers.ts
@@ -1,2 +1,2 @@
 // export const baseUrl = `http://localhost:9999`;
-export const baseUrl = `https://vote-tracker-6j7a-aier18x1f-eshunemmas-projects.vercel.app`;
+export const baseUrl = `https://vote.greatsohis.online`;
